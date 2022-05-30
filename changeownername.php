59674a2a148c43525aa40ec108ddd1533c0cfc1b
Sandy111111111 <92802749+Sandy111111111@users.noreply.github.com>
2022-05-30 22:56:32 +0530

    Create changeownername.php

---
+++ changeownername.php
@@ -0,0 +1,25 @@
+<?php
+$ownerName=$_POST['ownerName'];
+$vehicleNo=$_POST['vehicleNo'];
+$servername = 'localhost';
+$username = 'root';
+$password = '';
+$dbname = 'rto';
+
+// Create connection
+$conn = mysqli_connect($servername, $username, $password, $dbname);
+// Check connection
+if (!$conn) {
+    die("Connection failed: " . mysqli_connect_error());
+}
+
+$sql = "UPDATE vehicleregister_table SET ownerName='$ownerName' where vehicleNo='$vehicleNo'";
+//$sql = "SELECT firstname,lastname,email,mobile,gender,dob,address,city,pin,state,license,vehicle FROM licenseregister";
+$result = mysqli_query($conn, $sql);
+echo
+        "<script>alert('updated successfully');</script>";
+        include 'thankupage.html';
+
+
+mysqli_close($conn);
+?>
