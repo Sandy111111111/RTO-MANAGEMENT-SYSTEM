854b9fbf3bd0ea0bb0707f5f482bdd33e1133251
Sandy111111111 <92802749+Sandy111111111@users.noreply.github.com>
2022-05-30 22:59:21 +0530

    Create changeprofession.php

---
+++ changeprofession.php
@@ -0,0 +1,26 @@
+
+<?php
+$profession=$_POST['profession'];
+$vehicleNo=$_POST['vehicleNo'];
+$servername = 'localhost';
+$username = 'root';
+$password = '';
+$dbname = 'rto';
+
+// Create connection
+$conn = mysqli_connect($servername, $username, $password, $dbname);
+// Check connection
+if (!$conn) {
+    die("Connection failed: " . mysqli_connect_error());
+}
+
+$sql = "UPDATE vehicleregister_table SET profession='$profession' where vehicleNo='$vehicleNo'";
+//$sql = "SELECT firstname,lastname,email,mobile,gender,dob,address,city,pin,state,license,vehicle FROM licenseregister";
+$result = mysqli_query($conn, $sql);
+echo
+        "<script>alert('updated successfully');</script>";
+        include 'thankupage.html';
+
+
+mysqli_close($conn);
+?>
